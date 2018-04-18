8e0bec1f2dcea41359bce388a68cecd6dd6525ea
Hanna Lee <hannabeasiam@gmail.com>
2018-04-18 15:05:05 -0400

    template created, error with invoice_number in str, need to create invoice_header id and use for get_ instead of invoice_number

--- inc/dbconnect.php
+++ inc/dbconnect.php
@@ -2,23 +2,25 @@
 $servername = 'localhost';
 $dbname = 'invoices';
 $username = 'root';
-$password = 'Patryse3698'; //delete this later
+$password = 'Patryse3698'; // delete this later
 $dsn = "mysql:host=$servername;dbname=$dbname";
 
 try { 
   $db = new PDO($dsn, $username, $password);
+  $errorMessage = ''; // to check db connection works fine 
   $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
   // use exception to handle exeptional case (db not connectable)
-  echo "Unable to connect" .$e->getMessage();
+  $errorMessage = $e->getMessage();
   exit;
 }
 
-//seperate try catch block for each point of interaction
-try {
-  $db->query("SELECT invoice_number, customer_name FROM invoice_header"); // query method one argument (query as a string)
-  echo "Retrieved Results";
-} catch (Exception $e) {
-  echo "Unable to retrieved data";
-  exit;
+function display_db_error($errorMessage) {
+  echo '<aside>';
+  echo '<ul>';
+  echo "<li>$errorMessage</li>";
+  echo '</aside>';
 }
+
+
+
---
+++ inc/functions.php
@@ -0,0 +1,5 @@
+<?php
+function db_close() {
+  $db = NULL;
+}
+?>
